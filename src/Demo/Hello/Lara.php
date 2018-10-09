3393e6eb6b1558e1d4e12eef0ca5afd3389f11ac
3A532008 <isme1788@gmail.com>
2018-10-09 21:48:12 +0800

    Hello Lara

---
+++ src/Demo/Hello/Lara.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2018/10/9
+ * Time: 下午 09:36
+ */
+
+namespace Demo\Hello;
+
+class Lara
+{
+    function __construct()
+    {
+        echo 'Hello Lara~'.PHP_EOL;
+    }
+}
