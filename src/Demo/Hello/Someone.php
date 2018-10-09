eaf5aca1b550e23e94de8ded090e1a92cd43c81b
3A532008 <isme1788@gmail.com>
2018-10-09 21:48:41 +0800

    Hello Someone

---
+++ src/Demo/Hello/Someone.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2018/10/9
+ * Time: 下午 09:37
+ */
+
+namespace Demo\Hello;
+
+class Someone
+{
+    function __construct($name)
+    {
+        echo 'Hello '.$name.PHP_EOL;
+    }
+}
