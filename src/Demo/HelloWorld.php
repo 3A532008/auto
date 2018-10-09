00637ad46e35b20cdd128cc5bc8f2373c375bfc3
3A532008 <isme1788@gmail.com>
2018-10-09 21:47:21 +0800

    HelloWorld

---
+++ src/Demo/HelloWorld.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2018/10/9
+ * Time: 下午 09:35
+ */
+
+      namespace Demo;
+
+      class HelloWorld
+      {
+          function __construct()
+          {
+              echo 'Hello World ~'.PHP_EOL;
+          }
+      }
